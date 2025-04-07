73981d4b4d5f518f5385ee41c25a77635365759a
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2025-04-07 12:31:40 +0530

    users model changed

--- src/models/user.ts
+++ src/models/user.ts
@@ -29,7 +29,7 @@ export const UserSchema = new Schema(
     profilePhoto: { type: String },
     authCode: { type: String },
     loginType: { type: String, enum: ["researcher", "company", "admin"] },
-    about: { type: Array<String>, default: [] },
+    about: { type: String, default: "" },
     socialLink: { type: Array<String>, default: [] },
     isVerified: { type: Boolean, default: false },
     isDeleted: { type: Boolean, default: false },
